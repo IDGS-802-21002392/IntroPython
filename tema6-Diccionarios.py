ad90d4ac2d55a09e54a6de2f0f918e44ce8e892b
IDGS-802-21002392 <156359854+IDGS-802-21002392@users.noreply.github.com>
2024-01-16 13:29:44 -0600

    DLHR, 15 ENERO, Terminado el introPython

---
+++ tema6-Diccionarios.py
@@ -0,0 +1,12 @@
+'''
+
+'''
+
+miDiccionario={"Matricula":1234,"Nombre":"German","Apellidos":"Vargas"}
+print(miDiccionario["Nombre"])
+print(miDiccionario)
+print(type(miDiccionario))
+miDiccionario["Nombre"]="Juan"
+print(miDiccionario)
+miDiccionario["correo"]="correo@gmail.com"
+print(miDiccionario)
